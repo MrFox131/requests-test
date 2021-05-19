e748673c3742207241239a7247073c4f6d2a3f51
Koostyukov Maxim <enginer385@gmail.com>
2021-05-19 20:51:16 +1000

    Update req.py

--- req.py
+++ req.py
@@ -10,7 +10,7 @@ class Weather:
         if 200 <= res.status_code < 300:
             return True
 
-    def get_city_weather(self, city_name, lang, units):
+    def get_city_weather(self, city_name, lang="ru", units="metric"):
         query = {
             "q": city_name,
             "appid": self.token,
@@ -33,11 +33,22 @@ class Weather:
         return return_str
 
 
-city_name = input("Введите название города: ")
-lang = input("Введите язык, на котором необходимо вывести информацию (например, ru): ")
-units = input("Введите единицы измерения, в которых необхоимо вывести данные (например, metric): ")
+
 token = "799461e6591f3a6352bc3ef6727ecd2b"
 url = "https://api.openweathermap.org/data/2.5/weather"
 
 open_weather = Weather(token, url)
-print(open_weather.get_city_weather(city_name, lang, units))
+
+print("Введите 1, если хотите сделать запрос с параметрами по умолчани" \
+      "или любую другую цифру в ином случае: ")
+mode = input()
+
+city_name = input("Введите название города: ")
+
+if mode != "1":
+    lang = input("Введите язык, на котором необходимо вывести информацию (например, ru): ")
+    units = input("Введите единицы измерения, в которых необхоимо вывести данные (например, metric): ")
+    print(open_weather.get_city_weather(city_name, lang=lang, units=units))
+else:
+    print(open_weather.get_city_weather(city_name))
+
